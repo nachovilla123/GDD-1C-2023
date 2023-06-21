fbabb266394c7418b4c39a5ba4f140995f14d39a
nachovilla123 <nacho.villarruel@gmail.com>
2023-06-20 22:31:50 -0300

    practicando parciales anteriores

---
+++ parciales/parcial_19_11_22.sql
@@ -0,0 +1,14 @@
+I. Realizar una consulta SQL que permita saber los clientes que
+compraron en el 2012 al menos 1 unidad de todos los productos
+compuestos.
+De estos clientes mostrar, siempre para el 2012:
+I. El código del cliente
+2. Código de producto que en cantidades más compro.
+3. El número de fila según el orden establecido con un
+alias llamado ORDINAL.
+4. Cantidad de productos distintos comprados por el
+cliente.
+5. Monto total comprado.
+El resultado deberá ser ordenado por razón social del cliente
+alfabéticamente primero y luego, los clientes que compraron entre un
+20 % y 30% del total facturado en el 2012 primero, luego, los restantes.
