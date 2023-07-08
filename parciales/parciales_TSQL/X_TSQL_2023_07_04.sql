61733906687bb363085c65a9b371396608343de5
nachovilla123 <nacho.villarruel@gmail.com>
2023-07-07 21:38:57 -0300

    opcion de resuelto

--- parciales/parciales_TSQL/X_TSQL_2023_07_04.sql
+++ parciales/parciales_TSQL/X_TSQL_2023_07_04.sql
@@ -27,6 +27,109 @@ Se debe considerar que un producto compuesto puede estar compuesto por otros pro
 */
 
 
+
+
+
+
+/*
+Se requiere realizar una verificación de los precios de los COMBOS, 
+
+para ello se solicita que cree el o los objetos necesarios para realizar una operación que actualice que 
+
+el precio de un producto compuesto (COMBO) es el 90% de la suma de los precios de sus componentes por las cantidades que los componen. 
+
+Se debe considerar que un producto compuesto puede estar compuesto por otros productos compuestos.
+*/
+
+
+ALTER FUNCTION sumatoria_precio_componentes(@codigo_producto CHAR(8))
+RETURNS INT
+AS
+BEGIN
+	DECLARE @componente CHAR(8), @precio_final DECIMAL(12,2) = 0 ,@cantidad_componente INT ,@precio_componente DECIMAL(12,2)
+	
+	if exists( SELECT * FROM Composicion C where C.comp_producto = @codigo_producto )
+		begin
+			DECLARE el_cursor CURSOR FOR 
+				SELECT 
+				C.comp_componente, P.prod_precio,C.comp_cantidad
+				FROM Composicion C
+					INNER JOIN Producto P ON
+						P.prod_codigo = C.comp_componente
+				WHERE C.comp_producto = @codigo_producto
+	 
+			OPEN el_cursor
+			FETCH NEXT FROM el_cursor INTO  @componente, @precio_componente,@cantidad_componente
+			WHILE(@@FETCH_STATUS = 0)
+				BEGIN
+					IF NOT EXISTS(SELECT * FROM Composicion C1 WHERE C1.comp_producto = @componente)
+						BEGIN
+							SET @precio_final = @precio_final + @precio_componente*@cantidad_componente
+						END
+					ELSE
+						BEGIN
+							SET @precio_final = @precio_final + dbo.sumatoria_precio_componentes(@componente) * @cantidad_componente
+						END
+					FETCH NEXT FROM el_cursor INTO  @componente, @precio_componente,@cantidad_componente
+				END
+
+			CLOSE el_cursor
+			DEALLOCATE el_cursor
+		end
+
+	RETURN @precio_final
+END
+
+
+GO
+ALTER PROCEDURE verificar_cumplimiento_combos	-- SE DEBE EJECUTAR A MANO 1 VEZ PARA PONER AL DIA LA VERIFICACIÒN
+as
+BEGIN
+	declare @comp_producto char(8)		
+
+		DECLARE cd_cursor CURSOR FOR
+		SELECT 
+			C.comp_producto
+		FROM Producto P
+			INNER JOIN Composicion C
+				ON P.prod_codigo = C.comp_producto 
+	
+	OPEN cd_cursor
+		FETCH NEXT FROM cd_cursor INTO @comp_producto
+		while(@@FETCH_STATUS = 0)
+			begin
+				--acciones
+				UPDATE Producto
+				SET prod_precio = dbo.sumatoria_precio_componentes(@comp_producto) * 0.9
+				WHERE prod_codigo = @comp_producto;
+
+				FETCH NEXT FROM cd_cursor INTO @comp_producto
+			end
+	CLOSE cd_cursor
+	DEALLOCATE cd_cursor
+END
+
+
+GO
+alter TRIGGER trigerActualizadorProductos ON Producto AFTER INSERT
+AS
+BEGIN 
+	exec verificar_cumplimiento_combos
+END
+
+GO
+CREATE TRIGGER trigerActualizadorCombos ON Composicion AFTER INSERT,UPDATE
+AS
+BEGIN 
+	exec verificar_cumplimiento_combos
+END
+
+SELECT C.comp_producto,P.prod_precio FROM Composicion C INNER JOIN Producto P ON P.prod_codigo = C.comp_producto
+
+
+-- OTRA VERSION ABAJO:
+
+
 --interpreto que el control es manual para cada producto composicion.
 GO
 CREATE FUNCTION sumatoria_precio_componentes(@codigo_producto_compuesto char(8))
@@ -67,6 +170,7 @@ END
 
 
 
+------ si el ejericio fuera para corregir los productos composicion manualmente.
 GO
 CREATE PROCEDURE actualizarPrecioProductoComposicion (@producto_composicion char(8)) 
 AS
@@ -79,8 +183,38 @@ BEGIN
 END
 GO
 
+-- para actualizar todos los productos composicion en un proccedure
+GO
+CREATE PROCEDURE actualizarPrecioProductoComposicion () 
+AS
+BEGIN
+	declare @comp_producto char(8)
+	SET tRaNsACtiOn isolation LEVEL SERIALIZABLE
+
+		DECLARE mi_cursor CURSOR FOR
+		SELECT 
+			C.comp_producto
+		FROM Producto P
+			INNER JOIN Composicion C
+				ON P.prod_codigo = C.comp_producto 
+	
+	OPEN mi_cursor
+		FETCH NEXT FROM mi_cursor INTO @comp_producto
+		while(@@FETCH_STATUS = 0)
+			begin
+				UPDATE Producto
+				SET prod_precio = dbo.sumatoria_precio_componentes(@comp_producto) * 0.9
+				WHERE prod_codigo = @comp_producto;
+
+				FETCH NEXT FROM mi_cursor INTO @comp_producto
+			end
+	CLOSE mi_cursor
+	DEALLOCATE mi_cursor
+END
+GO
 
 
+-- problema: si actualizan la cantidad en composicion, no se da cuenta
 CREATE TRIGGER trigerActualizador ON Producto AFTER INSERT,UPDATE
 AS
 BEGIN 
@@ -129,3 +263,8 @@ INNER JOIN Producto P
 
 --select * from Composicion C2 
 --	join Producto on C2.comp_producto = prod_codigo
+
+
+
+
+
--- parciales/parciales_TSQL/X_TSQL_2023_07_04_TM.sql
+++ parciales/parciales_TSQL/X_TSQL_2023_07_04_TM.sql
@@ -7,3 +7,38 @@ foreign key entre ambos.
 NOTA: No se puede usar una foreign key para el ejercicio, deberá buscar
 otro método
 */
+
+-- nota de mi resolucion: unicamente checkea nuevas facturas y editadas. no se contemplan las facturas que estan en el sistema actualmente.
+
+ALTER TRIGGER vendedores
+ON FACTURA
+AFTER INSERT,UPDATE
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+	DECLARE @id_vendedor NUMERIC(6,0)
+
+	DECLARE facturas_insertadas CURSOR FOR
+		SELECT fact_vendedor FROM inserted
+
+	OPEN facturas_insertadas
+	FETCH NEXT FROM facturas_insertadas INTO @id_vendedor
+	
+		WHILE(@@FETCH_STATUS = 0)
+		BEGIN
+			IF NOT EXISTS (SELECT * FROM Empleado E WHERE E.empl_codigo = @id_vendedor)	-- SI 1 FACTURA NO CUMPLE SE CANCELA LA TRANSACCION
+				BEGIN
+					ROLLBACK TRANSACTION
+				END
+			FETCH NEXT FROM facturas_insertadas INTO @id_vendedor
+		END
+	CLOSE facturas_insertadas
+	DEALLOCATE facturas_insertadas
+
+END
+
+
+INSERT INTO Factura (fact_tipo,fact_sucursal,fact_numero,fact_fecha,fact_vendedor,fact_total,fact_total_impuestos,fact_cliente)
+	VALUES ('A','003','0006489','2010-01-23 00:00:00',NULL,'105.73','18.33','01634') 
+
+SELECT * FROM Factura
